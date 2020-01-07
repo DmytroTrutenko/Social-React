b97efbe93e323b020e1f77eb7d8b9c064013143d
DmytroTrutenko <55551711+DmytroTrutenko@users.noreply.github.com>
2020-01-07 16:19:08 +0200

    troubleshooting

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -3,54 +3,54 @@ import classes from './Users.module.css';
 
 let Users = (props) => {
 
-    var users = [{
-                id: 1,
-                photoURL: 'https://sun9-1.userapi.com/c636125/v636125322/13643/KkBYQow4QQI.jpg?ava=1',
-                followed: false,
-                fullname: 'Dmitry',
-                status: 'i am a boss',
-                location: {
-                    city: 'Tallinn',
-                    country: 'Estonia'
-                }
-            },
-            {
-                id: 2,
-                photoURL: 'https://sun9-71.userapi.com/c857224/v857224611/9c85/p8ULu-oNnXo.jpg?ava=1',
-                followed: true,
-                fullname: 'Vasia',
-                status: 'gg wp',
-                location: {
-                    city: 'Kiev',
-                    country: 'Ukraine'
-                }
-            },
-            {
-                id: 3,
-                photoURL: 'https://sun9-59.userapi.com/c851432/v851432612/151ff4/8KC8TLW1DhE.jpg?ava=1',
-                followed: true,
-                fullname: 'Vika',
-                status: 'i am a beautiful',
-                location: {
-                    city: 'Madrid',
-                    country: 'Spain'
-                }
-            },
-            {
-                id: 4,
-                photoURL: 'https://sun9-69.userapi.com/c845417/v845417469/d5a6f/7dXKtY3QyXY.jpg?ava=1',
-                followed: true,
-                fullname: 'Conor',
-                status: 'King UFC',
-                location: {
-                    city: 'New York',
-                    country: 'USA'
-                }
-            }]
-    if (props.users.lenght === 0) {
-        props.setUsers(users)
-    }
-
+if (props.users.length === 0){
+    props.setUsers([{
+        id: 1,
+        photoURL: 'https://sun9-1.userapi.com/c636125/v636125322/13643/KkBYQow4QQI.jpg?ava=1',
+        followed: false,
+        fullname: 'Dmitry',
+        status: 'i am a boss',
+        location: {
+            city: 'Tallinn',
+            country: 'Estonia'
+        }
+    },
+    {
+        id: 2,
+        photoURL: 'https://sun9-71.userapi.com/c857224/v857224611/9c85/p8ULu-oNnXo.jpg?ava=1',
+        followed: true,
+        fullname: 'Vasia',
+        status: 'gg wp',
+        location: {
+            city: 'Kiev',
+            country: 'Ukraine'
+        }
+    },
+    {
+        id: 3,
+        photoURL: 'https://sun9-59.userapi.com/c851432/v851432612/151ff4/8KC8TLW1DhE.jpg?ava=1',
+        followed: true,
+        fullname: 'Vika',
+        status: 'i am a beautiful',
+        location: {
+            city: 'Madrid',
+            country: 'Spain'
+        }
+    },
+    {
+        id: 4,
+        photoURL: 'https://sun9-69.userapi.com/c845417/v845417469/d5a6f/7dXKtY3QyXY.jpg?ava=1',
+        followed: true,
+        fullname: 'Conor',
+        status: 'King UFC',
+        location: {
+            city: 'New York',
+            country: 'USA'
+        }
+    }]);
+}
+       
+    
 
 
     return (
