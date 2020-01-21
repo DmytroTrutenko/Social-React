8086fcb6c36ab791b9ecc12c85d830fe30ed1160
DmytroTrutenko <55551711+DmytroTrutenko@users.noreply.github.com>
2020-01-22 01:48:10 +0200

    + library Reselect! and createSelector getUsers

--- src/components/Profile/ProfileInfo/ProfileStatus.jsx
+++ src/components/Profile/ProfileInfo/ProfileStatus.jsx
@@ -1,24 +1,23 @@
 import React from 'react';
 
-
 class ProfileStatus extends React.Component {
 
     state = {
         editMode: false,
         status: this.props.status
-    }
+    };
     activateEditMode = () => {
         this.setState({editMode: true});
-    }
+    };
     deactivateEditMode = () => {
         this.setState({editMode: false});
         this.props.updateUserStatus(this.state.status);
-    }
+    };
     onStatusChange = (e) => {
         this.setState({
             status: e.currentTarget.value
         });
-    }
+    };
 
     componentDidUpdate(prevProps, prevState) {
         //если предыдущий статус не такой как текущий статус что пришел при обновлении, то меняем
--- src/redux/users-selector.js
+++ src/redux/users-selector.js
@@ -1,15 +1,23 @@
-export const getUsers = (state) =>{
-  return state.usersPage.users;
+import {createSelector} from 'reselect';
+
+const getUsersSelector = (state) => {
+    return state.usersPage.users;
 };
-export const getPageSize = (state) =>{
+//создание селектора с помощью библиотеки reselect
+//будет перерисовка только когда изменится state в users
+export const getUsers = createSelector(getUsersSelector, users => {
+    return users.filter(u => true);
+});
+
+export const getPageSize = (state) => {
     return state.usersPage.pageSize;
 };
-export const getTotalUsersCount = (state) =>{
+export const getTotalUsersCount = (state) => {
     return state.usersPage.totalUsersCount;
 };
-export const getCurrentPage = (state) =>{
+export const getCurrentPage = (state) => {
     return state.usersPage.currentPage;
 };
-export const getFollowingInProgress = (state) =>{
+export const getFollowingInProgress = (state) => {
     return state.usersPage.followingInProgress;
 };
